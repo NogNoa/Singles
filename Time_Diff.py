a96d19b03299b012401143348545ef7b32c714fd
NogNoa <53447424+NogNoa@users.noreply.github.com>
2019-09-21 15:44:27 +0300

    Add files via upload

--- Time_Diff.py
+++ Time_Diff.py
@@ -4,15 +4,31 @@ Created on Sat Sep 21 12:50:19 2019
 
 @author: omer
 """
-def Timecnvrt(i):
+def PlceCnvrt(t):
+    
+
+def TimeCnvrt(i):
     i=str(i)
     mid=i.index(':')
     H=int(i[:mid])
-    M=int(i[mid+1:])
-    return H,M
+    i=i[mid+1:]
+    mid=i.find(':')
+    try:
+        M=int(i[:mid])        
+    except:
+        return H,0,0
+    if mid==-1:
+        return H,M,0
+    i=i[mid+1:]
+    mis=i.find(':')
+    if i!='':
+        S=i
+    else:
+        S=0
+    return H,M,S
     
 def SbtrctTime(E,L):
-    E=Timecnvrt(E)
-    L=Timecnvrt(L)
+    E=TimeCnvrt(E)
+    L=TimeCnvrt(L)
     return E,L
 print (SbtrctTime('16:52','5:02'))
