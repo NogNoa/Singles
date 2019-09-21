13edfe601f988f65816eb51fb85b8ae6b6fb06dd
NogNoa <53447424+NogNoa@users.noreply.github.com>
2019-09-21 16:52:15 +0300

    Add files via upload

--- Time_Diff.py
+++ Time_Diff.py
@@ -5,7 +5,12 @@ Created on Sat Sep 21 12:50:19 2019
 @author: omer
 """
 #def PlceCnvrt(t):
-
+def UntMnger(j):
+    for k in range(2):
+        j[k]+=j[k+1]//60
+        j[k+1]=j[k+1]%60
+    return j
+    
     
 def TimeCnvrt(i):
     i=str(i)
@@ -14,14 +19,22 @@ def TimeCnvrt(i):
     j=[int(k) for k in j]
     while len(j)<3:
         j.append(0)
-    for k in range(2):
-        j[k]+=j[k+1]//60
-        j[k+1]=j[k+1]%60
+    j=UntMnger(j)
     return j[0],j[1],j[2]
     
 def SbtrctTime(E,L):
     E=TimeCnvrt(E)
     L=TimeCnvrt(L)
     D=[L[i]-E[i] for i in range(3)]
-    return D
-print (SbtrctTime('16:67:64','5:02'))
+    if D[0]>0:
+        D=UntMnger(D)
+    else:
+        E=[abs(i) for i in D[1:]]
+        D=D[:1]+E
+    Ans=str(D[0])+':'+str(D[1])+':'+str(D[2])
+    return Ans
+print (SbtrctTime('16:67:64','0:97:60'))
+
+#mixed plusses and minuses 
+#say (-16:+30:-4) should be (-15:30:04)
+#now would be (-16:31:56)
