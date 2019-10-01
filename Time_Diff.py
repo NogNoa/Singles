cfd90f7c73d9c728f7a74ce7cda963fb4580e37b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2019-10-01 15:34:51 +0300

    Add files via upload
    If the later time, L is smaller than the earlier, you'll get answer for L in the next day, instead of negative time.

--- Time_Diff.py
+++ Time_Diff.py
@@ -10,8 +10,7 @@ def UntMnger(j):
         j[k]+=j[k+1]//60
         j[k+1]=j[k+1]%60
     return j
-    
-    
+       
 def TimeCnvrt(i):
     i=str(i)
     j=i.split(':')
@@ -23,13 +22,17 @@ def TimeCnvrt(i):
     return j[0],j[1],j[2]
     
 def SbtrctTime(E,L):
+    #E is for early and L is for late. 
+    #If L is smaller you'll get answer for L in the next day. 
     E=TimeCnvrt(E)
     L=TimeCnvrt(L)
-    sign=L[0]>E[0]
-    if sign:
-        D=[L[i]-E[i] for i in range(3)]
-    else:
-        D=[E[i]-L[i] for i in range(3)]
+    sign=L[0]>=E[0]
+    D=[L[i]-E[i] for i in range(3)]
+    if not sign:
+        D[0]+=24
+        lu=str(L[0])+':'+str(L[1])+':'+str(L[2])
+        print('for',lu,'in next day')
+        #helps the user notice that a 'day overflow' had occured
     D=UntMnger(D)
     Ans=str(D[0])+':'+str(D[1])+':'+str(D[2])
     return Ans
