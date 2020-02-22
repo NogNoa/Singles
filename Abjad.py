913b0bf728b09cc8ae0b4ecbe73ba62f18b2f463
NogNoa <53447424+NogNoa@users.noreply.github.com>
2020-02-22 12:46:43 +0200

    Abjad without list
    instead of generating a list version of the string to be mutated (a) just add whatever directly to output (b). makes for multiple points of exit and so slightly less clean, but less wastefull.

--- Abjad.py
+++ Abjad.py
@@ -6,7 +6,6 @@ Created on Sat Aug 31 19:47:07 2019
 """
 
 def abjad(Letter):
-    a=list(Letter)
     b=''
     Vowels=['a','e','i','o','u']
     MinorVow=['a','o','u']
@@ -17,11 +16,11 @@ def abjad(Letter):
         edge=Letter[i+1:i+2] in punct or Letter[i-1] in punct
         final=(j in MinorVow or cog) and not edge
         if final:
-            a[i]=''
+           b+=''
         elif Letter[i]=='h' and Letter[i-1] in Vowels:
-            a[i]="'h"
-        b+=a[i]
-    #for i in a:        
+           b+="'h"
+        else:
+            b+=j     
     return b
-print(abjad('A vowel is a syllabic speech sound pronounced without any stricture in the vocal tract. Vowels are one of the two principal classes of speech sounds, the other being the consonant. Vowels vary in quality, in loudness and also in quantity (length). They are usually voiced, and are closely involved in prosodic variation such as tone, intonation and stress.'))
-print(abjad('aahe'))
+#print(abjad('A vowel is a syllabic speech sound pronounced without any stricture in the vocal tract. Vowels are one of the two principal classes of speech sounds, the other being the consonant. Vowels vary in quality, in loudness and also in quantity (length). They are usually voiced, and are closely involved in prosodic variation such as tone, intonation and stress.'))
+#print(abjad('aahe'))
