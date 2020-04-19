679fd5730c2c1169b135f8c595fb8bbbd52fa55e
NogNoa <53447424+NogNoa@users.noreply.github.com>
2020-04-19 11:56:53 +0300

    Add files via upload
    revamped operator handeling. also generalized concacenating digits and operators (as plus or minus signs)
    problems:
    I'm relying on
        python's built-in math parsing which isn't really the idea
    loop and recursion don't work. Need to only have recursion.

--- arithmatic parsing.py
+++ arithmatic parsing.py
@@ -1,4 +1,10 @@
-g def parse(call):
+def parse(call):
+    def extend(operation,i):
+        nonlocal base, diff
+        if operation:
+            base+=i
+        else:
+            diff+=i
     call=str(call)
     operation=False
     base=''
@@ -6,27 +12,25 @@ g def parse(call):
     for i in call[::-1]:
         try:
             herring=int(i)
-            if operation:
-                base+=i
-            else:
-                diff+=i
+            extend(operation,i)
         except ValueError:
-            if operation:
-                back=int(base[::-1])+int(diff[::-1])
-                back=str(back)
-                if int(back)>0:
-                    back='+'+back
-                t=call.index(i)
-                call=call[:t+1]+back
-                #print(i, call)
-                parse(call)
-            else:
-                if i in {'+','-'}:
+            if i in {'+','-'}:
+                extend(operation,i)
+                if operation:
+                    back=int(base[::-1])+int(diff[::-1])
+                    back=str(back)
+                    t=call.index(i)
+                    call=call[:t]+back
+                    call=parse(call)
+                else:    
                     operation=True
-                    if i=='-':
-                        diff+='-'
     return call
-
 print(parse('7-3+14-26'))
 
 #'7-3+-12'
+"""
+by "extend(operation,i)" the operators I'm relying on
+    python's built-in math parsing which isn't really the idea
+loop and recursion don't work. we need only recursion.
+"""
+   
