2281cd2e979cd5b0d26de75b0d9e2c69b320b21c
NogNoa <53447424+NogNoa@users.noreply.github.com>
2020-05-29 11:59:00 +0300

    Add files via upload
    third iteration, with xclip

--- pswd-echo.sh
+++ pswd-echo.sh
@@ -1,3 +1,4 @@
+# /bin/bash
 if [ $(< /proc/sys/kernel/hostname) = WunderStatione ] 
 then 
     fl=/mnt/g/Data/ססמאות.csv
@@ -19,6 +20,6 @@ then
     q=$2
     p="2-3"
 fi
-cat $fl | grep $q | cut -d , -f $p | tr "," "\t"| tee clip.txt
+cat $fl | grep -i $q | cut -d , -f $p | tr "," "\t"| tee clip.txt
 xclip clip.txt && rm clip.txt
 #cut -d , -f 1 $fl
