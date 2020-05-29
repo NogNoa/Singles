85f3cd1b961d15ad41a01a62d4b3cb542afcaea8
NogNoa <53447424+NogNoa@users.noreply.github.com>
2020-05-29 11:55:07 +0300

    Add files via upload
    earliest version

---
+++ pswd-echo.sh
@@ -0,0 +1,22 @@
+if [ $(< /proc/sys/kernel/hostname) = $winhostname ] 
+then 
+    fl=/mnt/g/$tablepath.csv
+else
+    if [ $(< /proc/sys/kernel/hostname) = $linhostname ] 
+    then
+        fl=/mnt/Gate/$tablepath.csv
+    fi
+fi
+q=$1
+p="3"
+if [[ $1 = '--date' ]]
+then
+    stat $fl | grep Modify | cut -d ' ' -f 2
+    q=$2
+fi
+if [[ $1 = '-u' ]]
+then
+    p="2-3"
+fi
+cat $fl | grep $q | cut -d , -f $p | tr "," "\t"
+#cut -d , -f 1 $fl
