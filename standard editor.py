68a013d5b8010f36c2bc65c4612b2fb005d6c534
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-04-01 16:42:13 +0300

    Add files via upload

--- standard editor.py
+++ standard editor.py
@@ -1,62 +1,79 @@
-def insert():
-    insert = True
-    while insert:
-        line = input()
-        if line == ".":
-            insert = False
-        else:
-            stream.append(line + "\n")
-
-
-def expose(args):
-    try:
-        pl = int(args[0])
-    except IndexError:
-        for pl, line in enumerate(stream):
-            print(f"{pl}\t{line}")
-    except ValueError:
-        if args[0] == ".":
-            print(f"{focus}\t{stream[focus]}")
-        else:
-            print('?')
-    else:
-        print(f"{pl}\t{stream[pl]}")
-
-
-def chose(args):
+def parse(args):
     try:
         line = int(args[0])
     except IndexError:
-        print('?')
-        return focus
+        return '.'
     except ValueError:
-        print('?')
-        return focus
+        if args[0] in {'.', '*'}:
+            return args[0]
+        else:
+            return '?'
     else:
         return line
 
 
+def insert(mode):
+    if mode == '?':
+        print("?")
+        return
+    insert = True
+    cache = []
+    while insert:
+        line = input()
+        if line == ".":
+            insert = False
+            if mode == '*':
+                stream.extend(cache)
+            elif mode == '.':
+                stream[focus:focus] = cache
+            else:
+                stream[mode:mode] = cache
+        else:
+            cache.append(line + '\n')
+
+
+def expose(args):
+    arg = parse(args)
+    if arg == '*':
+        for pl, line in enumerate(stream):
+            print(f"{pl}\t{line}")
+    elif arg == '.':
+        print(f"{focus}\t{stream[focus]}")
+    elif arg == '?':
+        print('?')
+    else:
+        print(f"{arg}\t{stream[arg]}")
+
+
+def chose(args):
+    arg = parse(args)
+    if arg in {'*', '.', '?'}:
+        print('?')
+        return focus
+    else:
+        return arg
+
+
 def delete(args):
     global stream
-    try:
-        line = int(args[0])
-    except IndexError:
+    arg = parse(args)
+    if arg == '*':
         print('???')
         if input() == "y":
             stream.clear()
             # That's just evil.
-    except ValueError:
-        if args[0] == ".":
-            del stream[focus]
-        else:
-            print('?')
+    elif arg == '.':
+        del stream[focus]
+    elif arg == '?':
+        print('?')
     else:
-        del stream[line]
+        del stream[arg]
 
 
+name = "the standard.txt"
 run = True
 try:
-    with open("the standard.txt", "r+") as file:
+    with open(name, "r+") as file:
         stream = file.readlines()
 except FileNotFoundError:
     stream = []
@@ -65,11 +82,12 @@ focus = 0
 while run:
     line = input()
     if line == 'q':
-        with open("the standard.txt", "w+") as file:
+        with open(name, "w+") as file:
             file.write("".join(stream))
         run = False
-    elif line == 'i':
-        insert()
+    elif line[0] == 'i':
+        mode = parse(line[1:])
+        insert(mode)
     elif line[0] == 'p':
         line = line.split(' ')
         expose(line[1:])
