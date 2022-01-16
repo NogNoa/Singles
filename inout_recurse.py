55c25963fb4a2353f33c4b1b83627d5ab9d3113d
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-01-16 19:24:58 +0200

    line up

--- inout_recurse.py
+++ inout_recurse.py
@@ -1,5 +1,5 @@
 def sumin(L:list)->int:
-    return sumin(L[1:]) + L[0] if L else 0
+    return  sumin(L[1:])    + L[0]  if L else 0
     
 def sumout(L:list, sum=0)->int:
-    return sumout(L[1:],sum+L[0]) if L else sum
+    return sumout(L[1:],sum + L[0]) if L else sum
