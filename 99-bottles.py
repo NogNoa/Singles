ec0bca8d6d47327bdfa9897b1beff51f5577d107
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-15 22:14:29 +0200

    use lambda

--- 99-bottles.py
+++ 99-bottles.py
@@ -1,8 +1,8 @@
 for i in range(3,0,-1):
-	bottles= "bottle" + "s" * bool(i-1)
+	bottles= lambda j: "bottle" + "s" * bool(j-1)
 	print(
-f"""{i} {bottles} of beer on the wall
-{i} {bottles} of beer
+f"""{i} {bottles(i)} of beer on the wall
+{i} {bottles(i)} of beer
 Take one down, pass it around
-{i-1} bottles of beer on the wall
+{i-1} {bottles(i-1)} of beer on the wall
 """)
