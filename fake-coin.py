84075e5e69721162f7493666889cb921860609b2
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-06-29 18:01:04 +0300

    make test function

--- fake-coin.py
+++ fake-coin.py
@@ -15,23 +15,25 @@ def false_positive_rate(all: int, chosen: int) -> float:
     return case_probability(all, chosen, .5)
 
 
-if __name__ == "__main__":
-    accusation_treshold = .2
-    catch_target = .8
+def test(accusation_treshold: float, catch_target: float):
     satisfied = False
     flips = 1
     while not satisfied:
-        heads_treshold = 0
-        for heads_treshold in range(flips+1):
-            true_positives = [true_positive_rate(flips, heads) for heads in range(flips+1)]
-            false_positives = [false_positive_rate(flips, heads) for heads in range(flips+1)]
+        true_positives = [true_positive_rate(flips, heads) for heads in range(flips + 1)]
+        false_positives = [false_positive_rate(flips, heads) for heads in range(flips + 1)]
+        for heads_treshold in range(flips + 1):
             true_positives_sum = sum(true_positives[heads_treshold:])
             false_positives_sum = sum(false_positives[heads_treshold:])
             if true_positives_sum > catch_target and false_positives_sum < accusation_treshold:
-                print(f"Fliped {flips} coins.")
-                print(f"Accused players who got {heads_treshold}% or more heads.")
-                print(f"Accused {int(false_positives_sum*100)}% of fair players.")
-                print(f"Catched {int(true_positives_sum*100)}% of cheaters.")
-                satisfied = True
+                return flips, heads_treshold, true_positives_sum, false_positives_sum
         flips += 1
 
+
+if __name__ == "__main__":
+    def main():
+        flips, heads_treshold, tps, fps = test(.05, .8)
+        print(f"Fliped {flips} coins.")
+        print(f"Accused players who got {heads_treshold} or more heads.")
+        print(f"Accused {int(fps * 100)}% of fair players.")
+        print(f"Catched {int(tps * 100)}% of cheaters.")
+    main()
