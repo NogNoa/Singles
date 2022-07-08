e2f3d4561f824121a09073f2caddbbf3c8f426e0
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-07-08 16:33:38 +0300

    test multiple times

--- Prison_experiment.py
+++ Prison_experiment.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from fractions import Fraction
 
 def shuffled_boxes(num):
     import random
@@ -23,16 +23,27 @@ def search_boxes(prisoner: int, boxi: List[int], tries: int):
 
 
 if __name__ == "__main__":
-    def main(num=100):
+    def game(num=100, verbose=True):
         boxi = shuffled_boxes(num)
         for prisoner in range(num):
             success, opened = search_boxes(prisoner, boxi, num // 2)
             if success:
-                print(f"prisoner {prisoner} found his number, after checking: \n\t{opened}")
+                if verbose: print(f"prisoner {prisoner} found his number, after checking: \n\t{opened}")
             else:
-                print(f"Prisoner {prisoner} failed to find his number, after checking: \n\t{opened}")
-                break
+                if verbose: print(f"Prisoner {prisoner} failed to find his number, after checking: \n\t{opened}")
+                return False
+        else:
+            if verbose: print("All prisoners found their number!")
+            return True
 
+
+    def main(runs=1024, verbose=True):
+        successes = 0
+        for run in range(runs):
+            if game(verbose=False):
+                successes += 1
+        ratio = successes / runs
+        print(f"{successes} out of {runs}.\t{ratio:.2%} \t{Fraction(ratio)}")
 
 
     main()
