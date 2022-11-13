d01b1afded090882fb0b0f60d95860674b427a63
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-11-13 22:00:46 +0200

    sort tumblr

--- pic_clean.py
+++ pic_clean.py
@@ -3,4 +3,13 @@ import pathlib
 tumbler = pathlib.Path("C:/Users/Noga/downloads/A blog dedicated to all your favorite moments on Tumblr_files")
 fili = {}
 for file in tumbler.iterdir():
-    name, _, rest = file.name.partition("_")
+    tag, _, rest = file.name.partition("_")
+    if tag == "tumblr":
+        name, _, rest = rest.partition("_")
+        hash, _, rest = rest.partition("_")
+        size = int(rest.partition(".")[0])
+        try:
+            fili[name] += {hash: size}
+        except KeyError:
+            fili[name] = {hash: size}
+    elif tag == 
