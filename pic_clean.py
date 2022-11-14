bb94e9ad660fb47a8d1dcd6a5049886bb66b108b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-11-14 19:25:17 +0200

    simplify and unify structuring

--- pic_clean.py
+++ pic_clean.py
@@ -1,33 +1,25 @@
 import pathlib
 
-tumbler = pathlib.Path("C:/Users/Noga/downloads/A blog dedicated to all your favorite moments on Tumblr_files")
-fili = {"tumblr": {}, "other": []}
-for file in tumbler.iterdir():
-    tag, _, rest = file.name.partition("_")
-    if tag == "tumblr":
-        name, _, rest = rest.partition("_")
-        hash, _, rest = rest.partition("_")
-        size, _, ext = rest.partition(".")
-        size = int(size)
-        try:
-            fili["tumblr"][name][hash] = {"size": size, "ext": ext}
-        except KeyError:
-            fili["tumblr"][name] = {hash: {"size": size, "ext": ext}}
-    elif rest:
-        name, _, rest = rest.partition("_")
-        if rest:
-            size, _, ext = rest.partition(".")
-            size = int(size)
-            if tag in fili.keys():
-                if name in fili[tag].keys():
-                    fili[tag][name].append({"size": size, "ext": ext})
-                else:
-                    fili[tag][name] = [{"size": size, "ext": ext}]
-            else:
-                fili[tag] = {name: [size]}
+blog = pathlib.Path("C:/Users/Noga/downloads/A blog dedicated to all your favorite moments on Tumblr_files")
+fili = {"other": []}
+for file in blog.iterdir():
+    stem, ext = file.stem, file.suffix
+    tag, _, rest = stem.partition("_")
+    name, _, rest = rest.partition("_")
+    if not rest:
+        fili["other"].append(file.name)
+        continue
+    hash, sep, size = rest.partition("_")
+    if not size:
+        size, hash = hash, ""
+    hash += sep
+    size = int(size)
+    if tag in fili.keys():
+        if name in fili[tag].keys():
+            fili[tag][name].append({"hash": hash, "size": size, "ext": ext})
         else:
-            fili["other"].append(tag+"_"+name)
+            fili[tag][name] = [{"hash": hash, "size": size, "ext": ext}]
     else:
-        fili["other"].append(tag)
-with open("fili.json","w+") as codex:
+        fili[tag] = {name: [{"hash": hash, "size": size, "ext": ext}]}
+with open("fili.txt", "w+") as codex:
     codex.write(str(fili))
