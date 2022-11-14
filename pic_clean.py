a8d28ac618479692db51690244b1cd39958ab5f5
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-11-14 21:39:27 +0200

    style

--- pic_clean.py
+++ pic_clean.py
@@ -2,13 +2,13 @@ import pathlib
 
 def purge(name: str, value: list[dict]):
     for d in value[:-1]:
-        filo = blog / (name + '_' + d['hash'] + d['size'] + d['ext'])
+        filo = blog / f"{name}_{d['hash']}{d['size']}{d['ext']}"
         filo.unlink()
 
 blog = pathlib.Path("C:/Users/Noga/downloads/A blog dedicated to all your favorite moments on Tumblr_files")
 fili = {}
-for filo in blog.iterdir():
-    stem, ext = filo.stem, filo.suffix
+for file in blog.iterdir():
+    stem, ext = file.stem, file.suffix
     tag, _, rest = stem.partition("_")
     name, _, rest = rest.partition("_")
     if not rest:
@@ -18,15 +18,18 @@ for filo in blog.iterdir():
         size, hash = hash, ""
     hash += sep
     size = int(size)
-    if tag in fili.keys():
-        if name in fili[tag].keys():
+    try:  # to find tag in fili:
+        try:  # to find name in fili[tag]:
             fili[tag][name].append({"hash": hash, "size": size, "ext": ext})
-        else:
+        except KeyError:
             fili[tag][name] = [{"hash": hash, "size": size, "ext": ext}]
-    else:
+    except KeyError:
         fili[tag] = {name: [{"hash": hash, "size": size, "ext": ext}]}
+
+
 for tag in fili.keys():
-    for name in fili[tag].keys():
-        fili[tag][name].sort(key=(lambda n: n['size']))  # small first
-        purge(f"{tag}_{name}", fili[tag][name])
+    tag_dict = fili[tag]
+    for name in tag_dict.keys():
+        tag_dict[name].sort(key=(lambda n: n['size']))  # small first
+        purge(f"{tag}_{name}", tag_dict[name])
 
