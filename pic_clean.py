d7713a2a6bd48bb89e5b62c41272f8041403defd
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-11-14 19:39:52 +0200

    sort
    other is not really needed

--- pic_clean.py
+++ pic_clean.py
@@ -1,13 +1,12 @@
 import pathlib
 
 blog = pathlib.Path("C:/Users/Noga/downloads/A blog dedicated to all your favorite moments on Tumblr_files")
-fili = {"other": []}
+fili = {}
 for file in blog.iterdir():
     stem, ext = file.stem, file.suffix
     tag, _, rest = stem.partition("_")
     name, _, rest = rest.partition("_")
     if not rest:
-        fili["other"].append(file.name)
         continue
     hash, sep, size = rest.partition("_")
     if not size:
@@ -22,5 +21,6 @@ for file in blog.iterdir():
     else:
         fili[tag] = {name: [{"hash": hash, "size": size, "ext": ext}]}
 for tag in fili.values():
-    for name in tag:
+    for name in tag.values():
+        name.sort(key=(lambda n: n['size']))  # small first
         print(name)
