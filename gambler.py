0b3f31467debcd53ec2a4852ba4c4e19662902fb
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-01-06 21:07:03 +0200

    rest of gambler

--- gambler.py
+++ gambler.py
@@ -1,13 +1,44 @@
 import random
 
 
-def gamble(pocket: int, goal: int) -> int:
-    pot = (goal - pocket) // 10
+def bet(pot) -> int:
     roll = random.random()
+    back = 0
     if roll > .5:
-        pocket += pot
+        back += pot
     elif roll < .5:
-        pocket -= pot
+        back -= pot
+    return back
+
+
+def gamble(pocket: int, goal: int):
+    pot = (goal - pocket) // 10
+    pocket += bet(pot)
     return pocket
 
 
+def game(goal: int):
+    pocket = 1000
+    i = 0
+    while 0 <= pocket < goal - 10:
+        pocket = gamble(pocket, goal)
+        i += 1
+    return i, pocket
+
+
+def main():
+    num = 100
+    lens, result, jackpots = 0, 0, 0
+    for _ in range(num):
+        l, j = game(2000)
+        r = j > 0
+        lens += l
+        result += r
+        jackpots += j
+    lens = lens / num
+    result = result / num
+    jackpots = jackpots / num
+    print('lens: ', lens, '\nresult:', result, "\njackpot", jackpots)
+
+
+main()
