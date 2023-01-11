adfeb87a4da906506d1cb8d775ebba83e8db59cb
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-01-11 15:29:14 +0200

    fidgets and nodges

--- binary_diffs.py
+++ binary_diffs.py
@@ -1,10 +1,16 @@
 class BinDiff:
-    def __init__(self, start: int, vals: tuple[list[bytes], list[bytes]]):
+    def __init__(self, start: int, vals: tuple[bytes, bytes]):
         self.start = start
         self.vals = vals
 
     def __len__(self):
         return len(self.vals[0])
+
+    def __str__(self):
+        return f"{{len: {len(self)}, {str(self.vals)}"
+
+    def __repr__(self):
+        return str(self)
 
     @property
     def end(self):
@@ -26,6 +32,7 @@ def diff_find(scroll_nom: str, codex_nom: str) -> dict[int, BinDiff]:
     back = {}
 
     def cont():
+        nonlocal s, c
         while True:
             s = scroll.read(1)
             c = codex.read(1)
@@ -36,7 +43,7 @@ def diff_find(scroll_nom: str, codex_nom: str) -> dict[int, BinDiff]:
 
     while cont():
         start = scroll.tell()
-        scroll_val, codex_vall = [], []
+        scroll_val, codex_vall = bytes(), bytes()
         while s != c:
             scroll_val += s
             codex_vall += c
@@ -51,6 +58,7 @@ def diff_find(scroll_nom: str, codex_nom: str) -> dict[int, BinDiff]:
 def diff_reduce(scroll_nom: str, codex_nom: str, diffs: dict[int, BinDiff]):
     scroll = open(scroll_nom, "rb")
     codex = open(codex_nom, "rb")
+    deletes = []
 
     for diff in diffs.values():
         scroll.seek(diff.start)
@@ -58,8 +66,19 @@ def diff_reduce(scroll_nom: str, codex_nom: str, diffs: dict[int, BinDiff]):
         s = scroll.read(len(diff))
         c = codex.read(len(diff))
         if s != c:
-            del diffs[diff.start]
-
+            deletes.append(diff.start)
+    for d in deletes:
+        del diffs[d]
+    scroll.close()
+    codex.close()
     return diffs
 
 
+dir_nom = r"C:\Users\Noga\OneDrive\Documents\Electronic Arts\Dead Space\\"
+scroll_nom = dir_nom + "ds_slot_03.deadspacesaved"
+with open(r"D:\temp\DeadSpace MediumInsane Differences", "w+") as codex:
+    codex.write(str(
+                diff_reduce(
+                            scroll_nom, dir_nom + "ds_slot_01.deadspacesaved",
+                            diff_find(
+                                       scroll_nom, dir_nom + "ds_slot_04.deadspacesaved"))))
