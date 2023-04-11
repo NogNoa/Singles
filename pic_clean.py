b15c60b64e0f992e9bcce75d2f92a257ddef4dc5
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-04-12 01:03:20 +0300

    w/o tag_dict

--- pic_clean.py
+++ pic_clean.py
@@ -1,9 +1,11 @@
 import pathlib
 
+
 def purge(name: str, value: list[dict]):
     for d in value[:-1]:
         filo = blog / f"{name}_{d['hash']}{d['size']}{d['ext']}"
         filo.unlink()
+
 
 blog = pathlib.Path("C:/Users/Noga/downloads/A blog dedicated to all your favorite moments on Tumblr_files")
 fili = {}
@@ -26,10 +28,8 @@ for file in blog.iterdir():
     except KeyError:
         fili[tag] = {name: [{"hash": hash, "size": size, "ext": ext}]}
 
-
 for tag in fili.keys():
-    tag_dict = fili[tag]
-    for name in tag_dict.keys():
-        tag_dict[name].sort(key=(lambda n: n['size']))  # small first
-        purge(f"{tag}_{name}", tag_dict[name])
-
+    for name in fili[tag].keys():
+        versions = fili[tag][name]
+        versions.sort(key=(lambda n: n['size']))  # small first
+        purge(f"{tag}_{name}", versions)
