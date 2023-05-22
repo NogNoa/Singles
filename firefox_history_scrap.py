7a275548e886bbd72a3f612d5e271259e8417e51
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-05-22 11:17:54 +0300

    don't forget bak

--- firefox_history_scrap.py
+++ firefox_history_scrap.py
@@ -26,15 +26,17 @@ def lz_jsonify(scroll_nom: str):
     codex_nom = scroll_nom + ".json"
     if "upgrade" in scroll_nom:
         scroll_nom = "upgrade.jsonlz4-" + scroll_nom.partition("-")[2]
+    elif "bak" in scroll_nom:
+        scroll_nom += "lz4"
     else:
-        scroll_nom = scroll_nom + ".jsonlz4"
+        scroll_nom += ".jsonlz4"
     os.system(r"D:\Scripts\mozlz4.exe" + f" {scroll_nom} > {codex_nom}")
 
 
 if __name__ == "__main__":
     folder = r"C:\Users\Noga\AppData\Roaming\Mozilla\Firefox\Profiles\8e2xs3ha.default-release\sessionstore-backups"
     os.chdir(folder)
-    library = tuple(deleuze4(nom) for nom in os.listdir() if ".jsonlz4" in nom)
+    library = tuple(deleuze4(nom) for nom in os.listdir() if ".jsonlz4" in nom) + ("recovery.bak",)
     for scroll_nom in library:
         lz_jsonify(scroll_nom)
 
